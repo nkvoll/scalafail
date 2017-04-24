a1588d14811055a60538544da8b5c03fae5e2126
Njal Karevoll <njal@karevoll.no>
2017-04-24 23:51:22 +0200

    simplify nothing

--- src/main/scala/com/github/nkvoll/scalafail/nothing/NothingHmm.scala
+++ src/main/scala/com/github/nkvoll/scalafail/nothing/NothingHmm.scala
@@ -7,22 +7,20 @@ object NothingHmm {
   val mapper = new ObjectMapper()
 
   def main(args: Array[String]): Unit = {
-    val clusterData = mapper.createObjectNode
-    val repositoryNode = clusterData.putObject("snapshot").putObject("repository")
-    repositoryNode.put("name", "test-repository")
-    repositoryNode.putObject("config")
-      .put("type", "foo")
-      .putObject("settings").put("foo", "bar")
+    val rootNode = mapper.createObjectNode
+    val configNode = rootNode.putObject("foo").putObject("config")
 
-    //fails:
-    val expectedConfig = repositoryNode.path("config").deepCopy()
+    // compiles, fails at runtime:
+    val expectedConfig = rootNode.path("foo").deepCopy()
     /*
     Exception in thread "main" java.lang.ClassCastException: com.fasterxml.jackson.databind.node.ObjectNode cannot be cast to scala.runtime.Nothing$
 	at com.github.nkvoll.scalafail.nothing.NothingHmm$.main(NothingHmm.scala:17)
 	at com.github.nkvoll.scalafail.nothing.NothingHmm.main(NothingHmm.scala)
      */
 
-    // works:
-    val expectedConfig2: ObjectNode = repositoryNode.path("config").deepCopy()
+    // compiles and works:
+    val expectedConfig2: ObjectNode = rootNode.path("foo").deepCopy()
+
+    println(expectedConfig2)
   }
 }
